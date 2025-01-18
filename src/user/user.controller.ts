1ce741bef528602d3e7feb372c3eb75c05834efd
DevKike <alvaronarvaez.dev@gmail.com>
2025-01-17 23:15:16 -0500

    Refactor: practicing with body, post and query params

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,15 +3,15 @@ import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 @Controller('user')
 export class UserController {
   @Get('/:id')
-  getUser(@Param() params: any, @Query() query: any) {
-    console.log(`Params: ${JSON.stringify(params)}`);
-    console.log(`Query params: ${JSON.stringify(query)}`);
+  getUser(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'You sent a get request to user endpoint';
   }
 
   @Post()
-  createUser(@Body() req: any) {
-    console.log('🚀 ~ UserController ~ createUser ~ req:', req);
+  createUser(@Body('name') name: any) {
+    console.log(name);
     return 'You sent a post request to user endpoint';
   }
 }
