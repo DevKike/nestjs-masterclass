4e9a663dffc53ee697da73da73840fa8b24de358
DevKike <alvaronarvaez.dev@gmail.com>
2025-01-18 00:17:38 -0500

    Feat: working on params and query pipes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get('/:id')
-  getUser(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  getUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(limit);
+    console.log(page);
     return 'You sent a get request to user endpoint';
   }
 
