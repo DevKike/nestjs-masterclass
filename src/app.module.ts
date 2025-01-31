52d179c5f539c17e0d4112fdfb3b913ee4eca307
DevKike <alvaronarvaez151@gmail.com>
2025-01-31 01:01:00 -0500

    Feat: integrate TypeORM with PostgreSQL and create User entity for database interactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,33 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
+const IMPORTS = [
+  UserModule,
+  PostModule,
+  AuthModule,
+  TypeOrmModule.forRootAsync({
+    imports: [],
+    inject: [],
+    useFactory: () => ({
+      type: 'postgres',
+      entities: [User],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'Alvaro1516!',
+      host: 'localhost',
+      database: 'nestjs-blog',
+    }),
+  }),
+];
+const CONTROLLERS = [AppController];
+const PROVIDERS = [AppService];
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [...IMPORTS],
+  controllers: [...CONTROLLERS],
+  providers: [...PROVIDERS],
 })
 export class AppModule {}
