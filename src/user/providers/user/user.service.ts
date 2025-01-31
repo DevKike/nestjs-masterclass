c42e2036e0dfcd98e37ca3832b838969abee2ef4
DevKike <alvaronarvaez151@gmail.com>
2025-01-30 23:05:24 -0500

    Feat: enhance UserService with detailed method documentation and improve coverage metrics

--- src/user/providers/user/user.service.ts
+++ src/user/providers/user/user.service.ts
@@ -2,6 +2,10 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { AuthService } from 'src/auth/providers/auth/auth.service';
 import { GetUserParamsDTO } from 'src/user/dtos/get-user-params.dto';
 
+/**
+ * Class to connect to Users table and perform business operations
+ * @param {AuthService} _authService - The instance of the AuthService
+ */
 @Injectable()
 export class UserService {
   constructor(
@@ -9,6 +13,9 @@ export class UserService {
     private readonly _authService: AuthService,
   ) {}
 
+  /**
+   * The method to get all users to the database
+   */
   findAll(getUserParamsDTO: GetUserParamsDTO, limit: number, page: number) {
     const isAuth = this._authService.isAuth();
     console.log(isAuth);
@@ -24,6 +31,9 @@ export class UserService {
     ];
   }
 
+  /**
+   * The method to get a user by id
+   */
   findOneById(id: string) {
     return {
       id: id,
