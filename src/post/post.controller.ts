4a24b83eb643a424a4930bf667530aee14b67b0b
DevKike <alvaronarvaez151@gmail.com>
2025-01-30 22:37:04 -0500

    Feat: add PatchPostDTO for updating posts and implement updatePost method in PostController with Swagger documentation

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -4,11 +4,13 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { PostService } from './providers/post/post.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDTO } from './dtos/create-post.dto';
+import { PatchPostDTO } from './dtos/patch-post.dto';
 
 @Controller('post')
 @ApiTags('Post')
@@ -20,8 +22,27 @@ export class PostController {
     return this._postService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Create a post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if the post is created successfully',
+  })
   @Post('/')
   createPost(@Body() post: CreatePostDTO) {
     return this._postService.create(post);
+  }
+
+  @ApiOperation({
+    summary: 'Update a post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'You get a 200 response if the post is updated successfully',
+  })
+  @Patch()
+  updatePost(@Body() patchPostDTO: PatchPostDTO) {
+    console.log(patchPostDTO);
   }
 }
